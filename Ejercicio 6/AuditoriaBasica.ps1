b8eb687c708e9c3732d251a875953d1973a9b6d7
LaloT39 <eduardo.tamezo@uanl.edu.mx>
2025-08-16 00:47:59 -0600

    Add files via upload

---
+++ Ejercicio 6/AuditoriaBasica.ps1
@@ -0,0 +1,49 @@
+﻿Clear-Host 
+
+Import-Module AuditoriaBasica 
+
+ 
+
+Write-Host "=== Auditoría básica de usuarios y servicios ===" 
+
+Write-Host "1. Mostrar usuarios inactivos" 
+
+Write-Host "2. Mostrar servicios externos activos" 
+
+$opcion = Read-Host "Selecciona una opción (1 o 2)" 
+
+ 
+
+switch ($opcion) { 
+
+    "1" { 
+
+        $usuarios = Obtener-UsuariosInactivos 
+
+        $usuarios | Format-Table Name, Enabled, LastLogon -AutoSize 
+
+        $usuarios | Export-Csv -Path "C:\Users\lalot\Escritorio\Programacion\PC\users_inac.csv" -NoTypeInformation -ErrorAction Stop
+
+        Write-Host "`n📄 Reporte generado: ussers_inac.csv" 
+
+    } 
+
+    "2" { 
+
+        $servicios = Obtener-ServiciosExternos 
+
+        $servicios | Format-Table DisplayName, Status, StartType -AutoSize 
+
+        $servicios | ConvertTo-Html | Out-File "C:\Users\lalot\Escritorio\Programacion\PC\serv_e.html" -ErrorAction Stop
+
+        Write-Host "`n📄 Reporte generado: serv_e.html" 
+
+    } 
+
+    default { 
+
+        Write-Host "Opción no válida." 
+
+    } 
+
+} 
---
+++ Ejercicio 6/Modules/auditoriabasica.psm1
@@ -0,0 +1,35 @@
+﻿function Obtener-usuariosInactivos{
+<#
+.SYNOPSIS
+Obtiene los usuarios locales habilitados que nunca han inicado sesion.
+
+.DESCRIPTION
+Esta funcion busca cuentas locales habilitadas que no tienen ultima fecha de inicio de sesion.
+
+.EXAMPLE
+Obtener-UsuariosInactivos
+
+.NOTES
+Puede ayudar a detectar cuentas inecesarias o riesgosas en auditoias basicas.
+#>
+
+    Get-LocalUser | Where-Object{$_.Enabled -eq $true -and -not $_.LastLogon} -ErrorAction Stop
+}
+
+function Obtener-ServiciosExternos{
+<#
+.SYNOPSIS
+Obtiene los servicios en ejecucion que no pertenecen expliciamente a Windows.
+
+.DESCRIPTION
+Esta funcion filtra servicios activos cuyo nombre descriptivo no contiene el termino 'Windows'.
+
+.EXAMPLE
+Obtener-ServiciosExternos
+
+.NOTES
+Puede ayudar a detectar software de terceros en ejecucion en segundo plano.
+#>
+
+    Get-Service | Where-Object{$_.Status -eq "Running" -and $_.DisplayName -notmatch "Windows"}
+}
