ce1b8a567ce2f88b90bbd54848e6495d969e3647
LaloT39 <eduardo.tamezo@uanl.edu.mx>
2025-08-16 00:59:13 -0600

    Add files via upload

---
+++ Ejercicio 4/auditoria_usuarios.ps1
@@ -0,0 +1,21 @@
+﻿$usuarios= Get-LocalUser
+
+$SinLogin=@()
+$ConLogin=@()
+
+foreach($u in $usuarios){
+    if(-not $u.LastLogon){
+        $SinLogin += "$($u.Name): Estado = $($u.Enabled) , Ultimo accesso = NUNCA"
+    }else{
+        $ConLogin += "$($u.Name): Estado = $($u.Enabled) , Ultimo accesso = $($u.LastLogon)" 
+    }
+}
+
+$SinLogin | Out-File -FilePath "C:\Users\lalot\Escritorio\Programacion\PC\usuarios_sin_login.txt"
+$ConLogin | Out-File -FilePath "C:\Users\lalot\Escritorio\Programacion\PC\usuarios_con_login.txt"
+
+Write-Output "'n Usuarios que Nunca han iniciado sesion:"
+$SinLogin | Foreach-Object {Write-Output $_}
+
+Write-Output "'n Usuarios que si han iniciado sesion:"
+$ConLogin | Foreach-Object {Write-Output $_}
---
+++ Ejercicio 5/Verificar_archivos.ps1
@@ -0,0 +1,26 @@
+﻿function Validar-Archivo{
+    param(
+        [string]$ruta
+    )
+
+    try{
+        if(Test-Path $ruta){
+            $contenido = Get-Content $ruta -ErrorAction Stop
+            return "Archivo encontrado y accesible: $ruta"
+        }else{
+            throw "El archivo no existe"
+        }
+    }catch{
+        return "Error $_"
+    }finally{
+        Write-Host "Validacion finalizada par: $ruta" -ForegroundColor Cyan
+    }
+}
+
+Validar-Archivo -ruta "C:\archivo_inexistente.txt"
+
+Validar-Archivo -ruta "C:\Users\lalot\Escritorio\Programacion\PC\archivo.txt"
+
+
+$tamano = Get-ChildItem -Path "C:\Users\lalot\Escritorio\Programacion\PC\archivo.txt"
+Write-Host "El tamaño del archivo es >>> $($tamano.Length)" 
